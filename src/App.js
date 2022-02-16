efaafbf54b20ed0718e6b5d7cda4f77c48306c52
Victor <vitya898989@gmail.com>
2022-02-16 19:56:08 +0300

    Компонента

--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import './App.css';
 function App() {
     return (
         <div className="App">
+            <First />
+            <Html />
+        </div>
+    );
+}
+
+function First() {
+    return (
+        <div className="first">
+            first
+        </div>
+    );
+}
+
+function Html() {
+    return (
+        <div className="html">
             html
         </div>
     );
