927af3b0b0dce1f46a768be4ecb446e8dede7d20
Victor <vitya898989@gmail.com>
2022-02-16 19:35:32 +0300

    social pet origin repo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      html
-    </div>
-  );
+    return (
+        <div className="App">
+            html
+        </div>
+    );
 }
 
 export default App;
