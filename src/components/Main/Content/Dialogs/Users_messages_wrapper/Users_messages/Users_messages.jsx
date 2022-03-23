8da5d4415122ad821f559cc28a23c8506cae61e5
Victor <vitya898989@gmail.com>
2022-03-23 21:57:28 +0300

    social pet, добавление постов и сообщений часть 1

--- src/components/Main/Content/Dialogs/Users_messages_wrapper/Users_messages/Users_messages.jsx
+++ src/components/Main/Content/Dialogs/Users_messages_wrapper/Users_messages/Users_messages.jsx
@@ -1,12 +1,27 @@
 import style from './Users_messages.module.scss';
 import Users_message from "./Users_message/Users_message";
 import Empty_message from "./Empty_message/Empty_message";
+import React from 'react';
 
 const Users_messages = (props) => {
+
     let usersMessagesElements = (props.usersMessages.length > 0) ? props.usersMessages.map((userMessage) => <Users_message text={userMessage.text} incoming={userMessage.incoming} img={userMessage.img}/>) : <Empty_message />;
+
+    let newMessageEl = React.createRef();
+
+    let sendMessage = (e) => {
+        e.preventDefault();
+        let textMessage = newMessageEl.current.value;
+        alert(textMessage);
+    }
+
     return (
         <div className={style.users_messages}>
                 {usersMessagesElements}
+                <form className={style.users_messages_panel}>
+                    <textarea ref={newMessageEl}></textarea>
+                    <button onClick={ sendMessage }>Отправить</button>
+                </form>
         </div>
     );
 }
--- src/components/Main/Content/Profile/MyPostsWall/MyPostsWall.jsx
+++ src/components/Main/Content/Profile/MyPostsWall/MyPostsWall.jsx
@@ -1,18 +1,26 @@
 import style from './MyPostsWall.module.scss';
 import MyPost from "./MyPost/MyPost";
+import React from 'react';
 
 const MyPostsWall = (props) => {
 
     let myPostsElements = props.myPosts.map( myPost => <MyPost text={myPost.text} count_likes={myPost.countLikes} /> )
 
+    let newPost = React.createRef();
+
+    const addPost = () => {
+        let textContent = newPost.current.value;
+        alert(textContent);
+    };
+
     return (
         <div className={style.wall}>
             <div className={style.wall_title}>Моя стена</div>
             <div className={style.wall_form_wr}>
-                <form action="" name="wall_form">
-                    <textarea name="post" className={style.wall_textarea}></textarea>
+                <form action="">
+                    <textarea ref={ newPost } className={style.wall_textarea}></textarea>
                     <div className={style.wall_btn_wr}>
-                        <button className={style.wall_btn}>Отправить</button>
+                        <button className={style.wall_btn} onClick={ addPost }>Опубликовать</button>
                     </div>
                 </form>
             </div>
